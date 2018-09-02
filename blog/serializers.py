b29f471f3414e67c0c708a350816a81e81af2902
Borys Yudin <borysyudindm@gmail.com>
2018-09-02 20:35:27 +0300

    Change comments serializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -17,11 +17,12 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+
     class Meta:
         model = Comment
         fields = ('id', 'text', 'owner', 'post', 'created_at')
         extra_kwargs = {
             'id': {'read_only': True},
-            'owner': {'read_only': True},
             'created_at': {'read_only': True}
         }
